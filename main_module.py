006fcc55c4fc848b8a512a4e4fed0eba67ba9bf9
flashtechlearning <68813046+flashtechlearning@users.noreply.github.com>
2021-05-02 12:51:14 +0530

    Understanding main module and importing modules

---
+++ main_module.py
@@ -0,0 +1,11 @@
+import module1
+
+#print('__name__ is set to : {}'.format(__name__))
+
+
+def main():
+    print('Executing Main Function')
+    
+
+if __name__ == "__main__":
+    main()
---
+++ module1.py
@@ -0,0 +1,8 @@
+#print('The variable __name__ is set to : {}'.format(__name__))
+
+def module1_function():
+    print('Executing Module1 function')
+    
+    
+if __name__ == "__main__":
+    module1_function()
